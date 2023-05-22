cc1ad09f1b47bb62e7c36f7525f3742698855f15
choiyesol <126861784+choiyesol@users.noreply.github.com>
2023-05-22 17:32:46 +0900

    test5

---
+++ js/popup.js
@@ -0,0 +1,16 @@
+$(function () {
+    var img = $('img'),
+        imgshow = $('#imgshow'),
+        overly = $('#over'),
+        close = $('#close');
+        img.on('click', function () {
+        // overly 영역 보임
+        
+
+        imgshow.attr('src', $(this).attr('src'));
+
+        //close를 클릭하면 overly 영역 숨김
+        
+        
+  ;})
+;});
